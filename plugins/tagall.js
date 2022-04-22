14625a615eaea022753df22a3b84ff7741f29d90
hdiiofficial <79190751+hdiiofficial@users.noreply.github.com>
2022-04-23 03:36:33 +0700

    Update tagall.js

--- plugins/tagall.js
+++ plugins/tagall.js
@@ -1,7 +1,7 @@
 let handler = async(m, { conn, text, participants }) => {
-  let teks = `══✪〘 *👥 Mention All* 〙✪══\n\n➲ *Message : ${text ? text : 'Nothing'}*\n\n`
+  let teks = `══✪〘 *👥 Mention All* 〙✪══\n\n➲ *PESANNYA : ${text ? text : 'TANPA ALASAN'}*\n\n`
 		      	for (let mem of participants) {
-		            teks += `࿃➡️ @${mem.id.split('@')[0]}\n`
+		            teks += `࿃@${mem.id.split('@')[0]}\n`
 				}
                 teks += `\n⋙ *Admin Group* ⋘`
                 conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )
@@ -10,7 +10,9 @@ handler.help = ['tagall <pesan>']
 handler.tags = ['group']
 handler.command = /^(tagall)$/i
 
+handler.register = true
 handler.group = true
 handler.admin = true
+handler.botAdmin = true
 
 module.exports = handler
