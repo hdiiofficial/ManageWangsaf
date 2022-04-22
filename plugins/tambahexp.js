6da8dc70dd4b6428c328d862b3f12a61fce028a1
hdiiofficial <79190751+hdiiofficial@users.noreply.github.com>
2022-04-23 03:44:29 +0700

    Update tambahexp.js

--- plugins/tambahexp.js
+++ plugins/tambahexp.js
@@ -6,7 +6,7 @@ let cds = `${DeTik(new Date - LastClaim)}`
 let cd1 = Math.ceil(59 - cdm)
 let cd2 = Math.ceil(59 - cds)
   if (new Date - global.db.data.users[m.sender].lastclaim > 2700000) {
-    global.db.data.users[m.sender].exp += 100000
+    global.db.data.users[m.sender].exp += 1000
     m.reply('Nih kak expnya ✨100000\nSilahkan cek dengan mengetikkan .my')
     global.db.data.users[m.sender].lastclaim = new Date * 1
   } else throw `Kamu sudah mengambil exp hari ini silahkan.\n\nTunggu ${cd1} Menit ${cd2} Detik!`
