f679d0ad6b7a7ce56d8124f88e4df5fde5e22dfd
hdiiofficial <79190751+hdiiofficial@users.noreply.github.com>
2022-04-23 21:34:46 +0700

    Update main.js

--- main.js
+++ main.js
@@ -63,7 +63,7 @@ loadDatabase()
 // if (opts['cluster']) {
 //   require('./lib/cluster').Cluster()
 // }
-global.authFile = `./session/sessionnya.json`
+global.authFile = `./testbot/sesinya.json`
 global.isInit = !fs.existsSync(authFile)
 const { state, saveState } = useSingleFileAuthState(global.authFile)
 
