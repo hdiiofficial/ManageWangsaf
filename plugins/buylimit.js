088ac478b36d303d4811b13cf6911407e4b6f7c5
hdiiofficial <79190751+hdiiofficial@users.noreply.github.com>
2022-04-24 15:06:47 +0700

    Update buylimit.js

--- plugins/buylimit.js
+++ plugins/buylimit.js
@@ -1,10 +1,8 @@
-/**
-const xpperlimit = 350
+const xpperlimit = 350 
 let handler = async (m, { conn, command, args }) => {
   let count = command.replace(/^buy/i, '')
   count = count ? /all/i.test(count) ? Math.floor(global.db.data.users[m.sender].exp / xpperlimit) : parseInt(count) : args[0] ? parseInt(args[0]) : 1
   count = Math.max(1, count)
-  if (isNaN(count)) throw `hanya angka!\n\ncontoh: .buy5`
   if (global.db.data.users[m.sender].exp >= xpperlimit * count) {
     global.db.data.users[m.sender].exp -= xpperlimit * count
     global.db.data.users[m.sender].limit += count
@@ -12,13 +10,14 @@ let handler = async (m, { conn, command, args }) => {
   } else conn.reply(m.chat, `XP tidak mencukupi untuk membeli ${count} limit`, m)
 }
 handler.help = ['buy<jumlah limit>', 'buy <jumlah limit>', 'buyall']
-handler.tags = ['xp']
+handler.tags = ['rpg']
 handler.command = /^buy([0-9]+)|buy|buyall$/i
 handler.owner = false
 handler.mods = false
 handler.premium = false
 handler.group = false
 handler.private = false
+handler.register = true
 
 handler.admin = false
 handler.botAdmin = false
@@ -27,4 +26,3 @@ handler.fail = null
 handler.exp = 0
 
 module.exports = handler
-**/
