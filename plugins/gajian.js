cb20ab1ab269c7989d191eeb5cca13118faee1ec
hdiiofficial <79190751+hdiiofficial@users.noreply.github.com>
2022-04-24 14:49:00 +0700

    Update gajian.js

--- plugins/gajian.js
+++ plugins/gajian.js
@@ -5,8 +5,8 @@ let cds = `${DeTik(new Date - LastClaim)}`
 let cd1 = Math.ceil(44 - cdm)
 let cd2 = Math.ceil(59 - cds)
   if (new Date - global.db.data.users[m.sender].lastclaim > 2700000) {
-    global.db.data.users[m.sender].money += 7000
-    m.reply('Nih gaji lu +Rp7000')
+    global.db.data.users[m.sender].money += 2000
+    m.reply('Nih gaji lu +Rp2000')
     global.db.data.users[m.sender].lastclaim = new Date * 1
   } else throw `Lu udah ambil jatah hari ini.\n\nTunggu ${cd1} Menit ${cd2} Detik!`
 }
